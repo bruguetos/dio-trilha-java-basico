1dfeee9dba788f67459668cf4f639ccd800d3d92
bruguetos <rafaelmedeirosf@outlook.com>
2025-10-15 14:12:40 -0300

    Novo commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,6 +2,30 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        final double VALOR_DE_PI = 3.15;
+
+        String nomeCompleto = "Lingaguem" + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+
+        int a, b;
+        
+        a = 6;
+        b = 6;
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        
+  //  if (a==b)
+    //    resultado = "verdadeiro";
+    //else
+      //  resultado = "falso";
+
+
+
+
+        System.out.println(resultado);
+
     }
 }
