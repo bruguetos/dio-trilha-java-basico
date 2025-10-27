5e77ed5dd921a841772a25baddb32214e948155d
bruguetos <rafaelmedeirosf@outlook.com>
2025-10-27 11:31:38 -0300

    Commit

---
+++ JAVA-TERMINAL/src/AboutMe.java
@@ -0,0 +1,18 @@
+public class AboutMe {
+    
+        public static void main(String[] args) {
+            
+            String nome = args [0];
+            String sobrenome = args[1];
+            int idade = interger.valueOf(args[2]);
+            double altura = double.valueOf(args[3]);
+
+            System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha Altura é " + altura + "cm ");
+
+
+
+        }
+
+}
--- SISTEMA-SMART-TV/src/Usuario.java
+++ SISTEMA-SMART-TV/src/Usuario.java
@@ -8,7 +8,6 @@ public class Usuario {
         smartTv.diminuirVolume();
        
         smartTv.aumentarVolume();
-        
         System.out.println("Canal Atual: " + smartTv.canal);
 
         smartTv.mudarCanal(13);
